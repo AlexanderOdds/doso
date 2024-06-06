74d9d6d5bf7cf8b11649129b71ccfa4a11c62038
ภัทรดนัย คำไตรย์ <nongal3x@sMacBookAir.local>
2024-06-06 16:25:14 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import "./App.css"
+function App() {
+// const [task, setTask] = useState([]);
+// const addTask =(e) =>{
+  
+// }
+
+  return (
+    <>
+      <div className="flex-container">
+        <h1>Todo app</h1>
+        <div className="input-user">
+          <input id="taskInput" placeholder="Enter task here" type="text" />
+          <button id="addButton">Add</button>
+        </div>
+        <ul id="taskList">
+        </ul>
+      </div>
+    </>
+  );
+}
+export default App;
